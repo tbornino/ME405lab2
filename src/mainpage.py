daeb9e66935d0fafc11c8cdf63ec8aee7ba8debe
tbornino <vbornino@calpoly.edu>
2022-01-27 09:23:22 -0800

    Create mainpage.py
    Initial mainpage.py template

---
+++ src/mainpage.py
@@ -0,0 +1,20 @@
+'''!@file       mainpage.py
+    Main page for Lab 2: Closed Loop Controller Drivers.
+
+    @mainpage
+
+    @section sec_intro  Introduction
+                        This is the documentation site for Tori Bornino,
+                        Jackson Mclaughlin, and Zachary Stednitz's code used in
+                        ME 405 - Mechatronics, Lab 2: Closed loop controller 
+                        driver, taught in Winter 2022 by Dr. John Ridgly.
+                        Source code is available at:
+                        
+
+   
+
+    @author             Tori Bornino
+    @author             Jackson McLaughlin
+    @author             Zach Stednitz
+    @date               January 27, 2022
+'''
