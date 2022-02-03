b6795a8dae12dd41a9579981e9814425b2e632d9
tbornino <vbornino@calpoly.edu>
2022-02-03 10:38:27 -0800

    Update mainpage.py

--- src/mainpage.py
+++ src/mainpage.py
@@ -9,10 +9,22 @@
                         ME 405 - Mechatronics, Lab 2: Closed loop controller 
                         driver, taught in Winter 2022 by Dr. John Ridgly.
                         Source code is available at:
+                        https://tbornino.github.io/ME405lab2/
                         
 
-   
 
+    @section sec_con    PID Controller Class
+                        We wrote an PID Controller class
+                        @ref pidcontroller.PIDController "PIDController"
+                        that allows the user to run a step response on the motor.
+
+    @section sec_plt    Plotter Program on PC
+                        We wrote a low level user interface
+                        @ref plotter "plotter"
+                        that allows the user to input the step response gains
+                        and then prints the step response data after the step
+                        response. 
+   
     @author             Tori Bornino
     @author             Jackson McLaughlin
     @author             Zach Stednitz
